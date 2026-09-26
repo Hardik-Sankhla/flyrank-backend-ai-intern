34e1cc8be800ebdc7151f795c6f80c3df0445374
Hardik Sankhla <Hardik-Sankhla@users.noreply.github.com>
2026-09-26 03:07:19 +0000

    Sep 26, 2026, 8:37 AM

--- learning/basics/00variable.py
+++ learning/basics/00variable.py
@@ -1,7 +1,7 @@
 """Objective to learn about: 
-    - In Python we can make same variable name point to different types of objects
-    - learn about Type Casting
-    - To learn about 'type(Variable_name)' function 
+  Done  - In Python we can make same variable name point to different types of objects
+  Done  - learn about Type Casting
+  Done  - To learn about 'type(Variable_name)' function 
 
 """
 
