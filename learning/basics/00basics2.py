9a3a99dbab17da06016b8fec9d24de55a56e92bc
Hardik Sankhla <Hardik-Sankhla@users.noreply.github.com>
2026-09-26 09:02:50 +0530

    manual commit to test push

---
+++ learning/basics/00basics2.py
@@ -0,0 +1,25 @@
+"""Objectives :
+    - built-in data types & concepts of "Sequence" and "Containers"
+    - concept of mutablity and related Object ID
+    - Implicit and Explicit Type Casting
+    - Modules & Packages
+    - python strings, "Binary Literals" and Boolean types
+
+"""
+
+"""Data Types (Common)
+    - Numbers 
+        - Integers
+            - Boolean
+        - Floating Points
+        - Complex    
+    - None
+    - Sequences
+        - String
+        - Tuple
+        - List
+    - Sets
+    - Mappings
+        - Dictionary
+"""
+
