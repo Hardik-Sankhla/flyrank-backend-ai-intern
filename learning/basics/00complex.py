905176686a917d1f8ffc403f4083bd28f28f34a4
Hardik Sankhla <Hardik-Sankhla@users.noreply.github.com>
2026-09-26 08:21:09 +0000

    Sep 26, 2026, 1:51 PM

---
+++ learning/basics/00complex.py
@@ -0,0 +1,12 @@
+"""Objective: Complex Number - Simplify Complex Numbers With Python
+    - Complete the respective tutorials of complex numbers 
+        "https://realpython.com/python-complex-numbers/"
+    
+    Complex Number Example: z = 3+4j or mathamatically 3+4i, where 'i' is a imaginary number ex: i, -i, (pi)(i), (e)(i)
+
+    # Complex Number    
+    z= 5\ # Real Number
+          + 3i # imaginary number
+
+
+"""
